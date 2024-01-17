f541338250e632c12fef067cc4cb9ebe68eb956f
kholoudxs55kh <kfattem@gmail.com>
2024-01-17 18:18:31 +0200

    i knew it was wrong

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,7 @@
 -- SQL script that creates a table users
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US'
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255),
+       country ENUM ('US', 'CO', 'TN') NOT NULL
 );
